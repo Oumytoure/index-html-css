1ec4a547e23db1189b866c68e4deb884fe52b1bb
oumy <toureoumy12@gmail.com>
2022-08-29 17:15:27 +0000

    rectification tp1

--- toure.js
+++ toure.js
@@ -18,7 +18,31 @@ function afficherFormulaire() {
   var Email = document.getElementById("Email").value;
   var VotreMessage = document.getElementById("commentaire").value;
   var div = document.getElementById('Div1');
-div.innerHTML += Prenom;
+  const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/; 
+
+//controle de saisi du formulaire
+ let tableau =[
+  { PRENOM:Prenom, NOM:Nom, TELEPHONE:Telephone, EMAIL:Email
+
+ }];
+ console.table(tableau);
+ if(Nom.length === 0){
+  alert("veuillez saisir votre Prenom");
+ }
+ else if(Prenom.length ===0){
+  alert("veillez saisir Nom");
+ }
+ else if(Telephone.length ===0){
+  alert("veuillez saisir votre telephone");
+ }
+ else if(Email.length===0) {
+  alert("veuillez saisir votre email")
+ }
+ else if(!regexEmail.test(Email)){
+  alert("Veuillez saisir un email correct")
+ }
+ else{
+  div.innerHTML += Prenom;
 var div = document.getElementById('Div2');
 div.innerHTML += Nom;
 var div = document.getElementById('Div3');
@@ -27,11 +51,9 @@ var div = document.getElementById('Div4');
 div.innerHTML += Email;
 var div = document.getElementById('Div5');
 div.innerHTML += VotreMessage;
+  
+ }
+
 }
- 
- 
- 
-
-
 
 
